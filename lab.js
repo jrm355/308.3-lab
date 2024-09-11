f5f8587526fb56cb216804765e36bcaa3a21d18c
jrm355 <jeremykazdan34@gmail.com>
2024-09-10 20:17:14 -0400

    I had difficulty with this one

---
+++ lab.js
@@ -0,0 +1,51 @@
+//Part 1
+//fizz buzz
+
+//Loop through all numbers from 1 to 100.
+// for (let i = 0; i <= 100; i++) {
+// 	console.log(i);}
+// If a number is divisible by 3, log “Fizz.”
+//If a number is divisible by 5, log “Buzz.”//
+
+// for(let i=0; i<100; i++){
+// //     console.log((['Fizz'][i%3] || '') +
+// //     (['Buzz'][i%5] || '')) || i)
+//  } 
+
+ // part 2
+ //this is obviously a terrible way to do this it does technically work for a small amount of numbers
+//  for (let i = 10; i <= 30; i++) { 
+//         console.log(((['notprime'][i%2] || '') +
+//         (['notprime'][i%3] || '') +
+//         (['notprime'][i%4] || '') +
+//         (['notprime'][i%5] || '') 
+//         || i))
+ 
+
+// // Part 3
+// let wordcollection = [
+
+// ]
+
+// let temp = ''
+
+// ///let CSV = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
+// for (let i = 0; i < CSV.length; i++){
+// temp += CSV[i]
+// //if (CSV[i]=="\"}
+
+
+
+
+
+
+
+
+// let string = 
+// 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
+// for (let char of string) {
+
+
+
+    
+// }console.log (char)
